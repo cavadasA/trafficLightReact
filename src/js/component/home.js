6a91e73b58cf3e3e37558f51bf95b3968922ae2a
cavadasA <antonio.cavadas.gu@gmail.com>
2021-06-09 21:46:38 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,38 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	const [color, setColor] = useState("");
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="row justify-content-center mt-5">
+			<div className="col-1 bg-dark px-5 py-5 lights">
+				<div className="row py-3">
+					<span
+						className={
+							"red-dot" + (color === "red" ? " selected" : "")
+						}
+						onClick={() => setColor(() => "red")}></span>
+				</div>
+				<div className="row py-3">
+					<span
+						className={
+							"yellow-dot" +
+							(color === "yellow" ? " selected" : "")
+						}
+						onClick={() => setColor(() => "yellow")}></span>
+				</div>
+				<div className="row py-3">
+					<span
+						className={
+							"green-dot" + (color === "green" ? " selected" : "")
+						}
+						onClick={() => setColor(() => "green")}></span>
+				</div>
+			</div>
 		</div>
 	);
 }
